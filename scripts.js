46b17944b2f79e9daa133ec816c669a36c576d35
RafaelNaCl <rafaelgraffeo@hotmail.ch>
2025-04-01 19:28:41 +0000

    rewrk

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+    console.log("Ginkoblatt-Animation gestartet");
+  
     const totalFrames = 9;
     let currentFrame = 1;
   
@@ -6,11 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
       if (currentFrame > 1) {
         document.querySelector(`.frame${currentFrame - 1}`).style.opacity = 0;
       }
+  
       if (currentFrame <= totalFrames) {
         document.querySelector(`.frame${currentFrame}`).style.opacity = 1;
         currentFrame++;
       } else {
         clearInterval(interval);
       }
-    }, 180); // 180ms pro Bild = ca. 1.6 Sekunden Gesamtlaufzeit
+    }, 180); // 180ms pro Bild = ca. 1.6 Sekunden
   });
