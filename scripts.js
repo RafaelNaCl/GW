749fed08e8852d64a2ab96a8d519b95a48c79b58
RafaelNaCl <rafaelgraffeo@hotmail.ch>
2025-04-01 19:07:57 +0000

    Vorbereitung

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const totalFrames = 9;
+    let currentFrame = 1;
+  
+    const interval = setInterval(() => {
+      if (currentFrame > 1) {
+        document.querySelector(`.frame${currentFrame - 1}`).style.opacity = 0;
+      }
+      if (currentFrame <= totalFrames) {
+        document.querySelector(`.frame${currentFrame}`).style.opacity = 1;
+        currentFrame++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 180); // 180ms pro Bild = ca. 1.6 Sekunden Gesamtlaufzeit
+  });
