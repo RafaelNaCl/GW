672c270dc74a3b367ec4aea184adf046ccd20813
RafaelNaCl <rafaelgraffeo@hotmail.ch>
2025-04-03 11:55:00 +0000

    webdesigner test

--- scripts.js
+++ scripts.js
@@ -1,24 +1,18 @@
-console.log("DOM vollständig geladen");
 document.addEventListener("DOMContentLoaded", () => {
   const frames = document.querySelectorAll(".blatt-frame");
   const totalFrames = frames.length;
   let current = 0;
 
-  // Initialzustand: alle ausblenden
-  frames.forEach(frame => frame.style.opacity = 0);
-
   function animateFrames() {
-    console.log("Animation gestartet");
+    frames.forEach(f => f.classList.remove("active"));
+    if (frames[current]) {
+      frames[current].classList.add("active");
+    }
+    current++;
     if (current < totalFrames) {
-      frames.forEach(f => f.classList.remove("active")); // Alle Frames unsichtbar
-      if (frames[current]) {
-        frames[current].classList.add("active"); // Aktuellen Frame aktiv setzen
-      }
-
-      current++;
       setTimeout(animateFrames, 180);
     }
   }
 
-  animateFrames(); // Start der Animation
+  animateFrames();
 });
